d65db6c01247578c5839f9b701b2a812736321ed
MCollaresR <89664004+MCollaresR@users.noreply.github.com>
2022-07-11 14:13:10 -0300

    Gera pdf com base no HTML

---
+++ leitor_html.py
@@ -0,0 +1,3 @@
+import pdfkit as pk
+
+pk.from_file('/home/elias/Documentos/Analise_Empresas_Ativas/analise_jucema/leitura_jucema/graficos/qtd_empresarios.html', '/home/elias/Documentos/Analise_Empresas_Ativas/analise_jucema/leitura_jucema/pdfs/Qtd_Empresarios.pdf') 
